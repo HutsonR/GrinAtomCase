2ea37c2927ede1b5d6d1ff3fd22e52c24a3e1fd9
Roman <hutsonr155@gmail.com>
2023-08-22 17:08:25 +0300

    del hand test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Solutions.ReverseLinkedList;
 public class Main {
     public static void main(String[] args) {
 //        Solutions.SwapVariables.test();
-        ReverseLinkedList.test();
-        Palindrome.test();
+//        ReverseLinkedList.test();
+//        Palindrome.test();
     }
 }
