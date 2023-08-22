6b54ef72a03e2501978fcb9daa6c54b2083bfd6d
Roman <hutsonr155@gmail.com>
2023-08-22 17:08:44 +0300

    add auto test

--- src/Tests/PalindromeTest.java
+++ src/Tests/PalindromeTest.java
@@ -1,4 +1,38 @@
 package Tests;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import Solutions.Palindrome;
 
 public class PalindromeTest {
+
+    @Test
+    public void testIsPalindromeWithValidPalindrome() {
+        assertTrue(Palindrome.isPalindrome("racecar"));
+    }
+
+    @Test
+    public void testIsPalindromeWithValidNonPalindrome() {
+        assertFalse(Palindrome.isPalindrome("hello"));
+    }
+
+    @Test
+    public void testIsPalindromeWithEmptyString() {
+        assertTrue(Palindrome.isPalindrome(""));
+    }
+
+    @Test
+    public void testIsPalindromeWithSingleCharacter() {
+        assertTrue(Palindrome.isPalindrome("a"));
+    }
+
+    @Test
+    public void testIsPalindromeWithWhitespace() {
+        assertTrue(Palindrome.isPalindrome("  "));
+    }
+
+    @Test
+    public void testIsPalindromeWithMixedCharacters() {
+        assertTrue(Palindrome.isPalindrome("A man, a plan, a canal, Panama"));
+    }
 }
+
