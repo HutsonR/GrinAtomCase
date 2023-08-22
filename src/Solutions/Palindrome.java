2da6dfd9db269b2b9e529a2653d437365b8752c2
Roman <hutsonr155@gmail.com>
2023-08-22 17:03:04 +0300

    add task palindrome

---
+++ src/Solutions/Palindrome.java
@@ -0,0 +1,43 @@
+package Solutions;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void test() {
+        Scanner scan = new Scanner(System.in);
+        String s = null;
+        System.out.print("Введите слово: ");
+        s = scan.nextLine();
+        System.out.println(isPalindrome(s));
+    }
+
+    private static boolean isPalindrome(String s) {
+        if (s.isEmpty() || s.equals(" ") || s.length() == 1) {
+            return true;
+        }
+
+        int start = 0;
+        int end = s.length()-1;
+        Boolean isResult = true;
+        while (start <= end) {
+            char curStartSym = s.charAt(start);
+            char curEndSym = s.charAt(end);
+            if (!Character.isLetterOrDigit(curStartSym)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(curEndSym)) {
+                end--;
+            } else {
+                if (Character.toLowerCase(curStartSym) == Character.toLowerCase(curEndSym)) {
+                    isResult = true;
+                    start++;
+                    end--;
+                    continue;
+                }
+                isResult = false;
+                break;
+            }
+        }
+
+        return isResult;
+    }
+}
