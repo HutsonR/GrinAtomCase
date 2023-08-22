658c9e53cdd640c6e5a426e7beb1006046df78e3
Roman <hutsonr155@gmail.com>
2023-08-22 17:14:28 +0300

    del hand test

--- src/Solutions/ReverseLinkedList.java
+++ src/Solutions/ReverseLinkedList.java
@@ -1,24 +1,7 @@
 package Solutions;
 
-import java.util.LinkedList;
-
 public class ReverseLinkedList {
-    public static void test() {
-        ListNode list = new ListNode(5);
-        list.next = new ListNode(65);
-        list.next.next = new ListNode(14);
-        list.next.next.next = new ListNode(78);
-
-        System.out.print("Список до изменения: ");
-        printList(list);
-        System.out.println(); // переход на новую строку
-        list = reverseList(list);
-        System.out.print("Список после изменения: ");
-        printList(list);
-        System.out.println();
-    }
-
-    private static ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode prev = null;
 
@@ -33,19 +16,11 @@ public class ReverseLinkedList {
         return head;
     }
 
-    private static void printList(ListNode node)
-    {
-        while (node != null) {
-            System.out.print(node.val + " ");
-            node = node.next;
-        }
-    }
-
-    private static class ListNode {
-      private int val;
-      private ListNode next;
+    public static class ListNode {
+      public int val;
+      public ListNode next;
       ListNode() {}
-      ListNode(int val) { this.val = val; }
+      public ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
