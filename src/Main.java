ae0913e8c4f9bfb7882b7e193e04b0fa27101fb6
Roman <hutsonr155@gmail.com>
2023-08-22 17:03:56 +0300

    moved to package

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Solutions.Palindrome;
+import Solutions.ReverseLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        SwapVariables.test();
+//        Solutions.SwapVariables.test();
         ReverseLinkedList.test();
+        Palindrome.test();
     }
 }
