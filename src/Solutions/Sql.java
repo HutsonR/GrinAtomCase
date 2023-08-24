2559ebea30a2ae128a5dc0d936747bceeb50e790
Roman <hutsonr155@gmail.com>
2023-08-24 19:51:54 +0300

    add sql query

---
+++ src/Solutions/Sql.java
@@ -0,0 +1,8 @@
+package Solutions;
+
+public class Sql {
+    String query = "SELECT surname " +
+            "FROM table " +
+            "ORDER BY experience DESC " +
+            "OFFSET 1 LIMIT 1";
+}
