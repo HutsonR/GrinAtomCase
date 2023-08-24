32287f723556dc8b26573ebb8638f14f6037715b
Roman <hutsonr155@gmail.com>
2023-08-24 18:33:58 +0300

    add java doc

--- src/Solutions/ReverseLinkedList.java
+++ src/Solutions/ReverseLinkedList.java
@@ -1,6 +1,16 @@
 package Solutions;
 
+/**
+ * The ReverseLinkedList class provides methods for reversing a singly linked list.
+ */
 public class ReverseLinkedList {
+
+    /**
+     * Reverses a given singly linked list.
+     *
+     * @param head The head of the linked list to be reversed.
+     * @return The head of the reversed linked list.
+     */
     public static ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode prev = null;
@@ -10,17 +20,42 @@ public class ReverseLinkedList {
             current.next = prev;
             prev = current;
             current = nextNode;
-
         }
         head = prev;
         return head;
     }
 
+    /**
+     * Definition for singly-linked list node.
+     */
     public static class ListNode {
-      public int val;
-      public ListNode next;
-      ListNode() {}
-      public ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        public int val;
+        public ListNode next;
+
+        /**
+         * Default constructor for a ListNode.
+         */
+        public ListNode() {}
+
+        /**
+         * Constructs a ListNode with a given value.
+         *
+         * @param val The value of the node.
+         */
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        /**
+         * Constructs a ListNode with a given value and the next node.
+         *
+         * @param val The value of the node.
+         * @param next The next node in the linked list.
+         */
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
+
