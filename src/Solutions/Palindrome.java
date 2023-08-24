610968345fe957923c8fe4b8d3e734d800e615f7
Roman <hutsonr155@gmail.com>
2023-08-24 18:35:47 +0300

    add java doc

--- src/Solutions/Palindrome.java
+++ src/Solutions/Palindrome.java
@@ -2,15 +2,25 @@ package Solutions;
 
 import java.util.Scanner;
 
+/**
+ * The Palindrome class provides a method to check if a given string is a palindrome.
+ */
 public class Palindrome {
+
+    /**
+     * Checks if the given string is a palindrome.
+     *
+     * @param s The input string to be checked.
+     * @return {@code true} if the string is a palindrome, {@code false} otherwise.
+     */
     public static boolean isPalindrome(String s) {
         if (s.isEmpty() || s.equals(" ") || s.length() == 1) {
             return true;
         }
 
         int start = 0;
-        int end = s.length()-1;
-        Boolean isResult = true;
+        int end = s.length() - 1;
+        boolean isResult = true;
         while (start <= end) {
             char curStartSym = s.charAt(start);
             char curEndSym = s.charAt(end);
@@ -33,3 +43,4 @@ public class Palindrome {
         return isResult;
     }
 }
+
