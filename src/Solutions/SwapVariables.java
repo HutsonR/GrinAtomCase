4c0015592e66b80b5f52aef20f9dadec138b4739
Roman <hutsonr155@gmail.com>
2023-08-24 18:48:20 +0300

    add java doc

--- src/Solutions/SwapVariables.java
+++ src/Solutions/SwapVariables.java
@@ -1,6 +1,17 @@
 package Solutions;
 
+/**
+ * The SwapVariables class provides a method for swapping two integer values.
+ */
 public class SwapVariables {
+
+    /**
+     * Swaps the values of two integers.
+     *
+     * @param a The first integer value.
+     * @param b The second integer value.
+     * @return An array containing the swapped values: [newA, newB].
+     */
     public static int[] swap(int a, int b) {
         a = a + b;
         b = a - b;
