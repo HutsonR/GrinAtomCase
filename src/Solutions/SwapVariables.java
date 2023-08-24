a6c94f7f92aba36165b7525d6709ebdf40da7af4
Roman <hutsonr155@gmail.com>
2023-08-24 18:39:14 +0300

    del hand test

--- src/Solutions/SwapVariables.java
+++ src/Solutions/SwapVariables.java
@@ -3,16 +3,6 @@ package Solutions;
 import java.util.Scanner;
 
 public class SwapVariables {
-    public static void test() {
-        Scanner scan = new Scanner(System.in);
-        int a, b;
-        System.out.print("Введите a: ");
-        a = scan.nextInt();
-        System.out.print("Введите b: ");
-        b = scan.nextInt();
-        swap(a, b);
-    }
-
     public static void swap(int a, int b) {
         a = a + b;
         b = a - b;
