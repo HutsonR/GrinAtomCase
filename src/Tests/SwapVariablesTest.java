51ab2624e427c1cb5a00295192fd64e305c4f1b5
Roman <hutsonr155@gmail.com>
2023-08-24 18:43:06 +0300

    add auto test

---
+++ src/Tests/SwapVariablesTest.java
@@ -0,0 +1,52 @@
+package Tests;
+
+import Solutions.SwapVariables;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SwapVariablesTest {
+    @Test
+    public void testSwap() {
+        int a = 5;
+        int b = 10;
+
+        int[] result = SwapVariables.swap(a, b);
+
+        assertEquals(b, result[0]);
+        assertEquals(a, result[1]);
+    }
+
+    @Test
+    public void testSwapWithNull() {
+        int a = 0;
+        int b = 0;
+
+        int[] result = SwapVariables.swap(a, b);
+
+        assertEquals(b, result[0]);
+        assertEquals(a, result[0]);
+    }
+
+    @Test
+    public void testSwapWithSameValues() {
+        int a = 7;
+        int b = 7;
+
+        int[] result = SwapVariables.swap(a, b);
+
+        assertEquals(b, result[0]);
+        assertEquals(a, result[1]);
+    }
+
+    @Test
+    public void testSwapWithNegativeValues() {
+        int a = -3;
+        int b = -8;
+
+        int[] result = SwapVariables.swap(a, b);
+
+        assertEquals(b, result[0]);
+        assertEquals(a, result[1]);
+    }
+}
