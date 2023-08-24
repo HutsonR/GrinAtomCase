e4eafab9da24ea386955ec62c5bd2cf5ced5176e
Roman <hutsonr155@gmail.com>
2023-08-24 18:47:15 +0300

    empty main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,5 @@ import Solutions.Palindrome;
 import Solutions.ReverseLinkedList;
 
 public class Main {
-    public static void main(String[] args) {
-//        Solutions.SwapVariables.test();
-//        ReverseLinkedList.test();
-//        Palindrome.test();
-    }
+    public static void main(String[] args) {}
 }
